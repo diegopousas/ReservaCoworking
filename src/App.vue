0d561c50c92dae9a0e0d10c2bd49d1cb0b5126fe
diegopousas <diego.vieira@demarco.com.br>
2023-07-19 17:00:28 -0300

    Validações e Funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <h1>Agendamento Coworking</h1>
     <p>Qual a data que deseja agendar ? <input type="date" class="inpData" v-model="data"></p>
     <p>Informe seu nome: <input type="text" v-model="nome"></p>
-    <p><button @click="reservar">Salvar</button> <button>Limpar</button> <button @click="cancelar">Cancelar Reserva</button></p>
-    <p v-if="reservas.length == 10" style="color:red; font-size: 13px;">O número de vagas disponiveis esgotou.</p>
-    <p v-else style="color: gray; font-size: 13px;">Restam {{ 10 - reservas.length }} vaga(s) disponíveis.</p>
+    <p><button @click="reservar">Salvar</button> <button @click="mostrar = !mostrar">Cancelar Reserva</button></p>
+    <p v-if="reservas.length == 10" style="color:red; font-size: 13px;font-weight: bold;">O número de vagas disponiveis esgotou.</p>
+    <p v-if="reservas.length == 10" style="color:green; font-size: 13px;">Você pode realizar uma reserva e seu nome estará na lista espera de uma desistência.</p>
+    <p v-else style="color: gray; font-size: 13px;">Restam <strong>{{ 10 - reservas.length }}</strong> vaga(s) disponíveis.<br>Após preenchidas, abrimos uma lista de espera.</p>
     <hr>
-    <p>
-      <ul class="lista">
-        <li v-for="reserva of reservas">{{ reserva }}</li>
-      </ul>
-    </p>
+    <div class="abaCancelamento" v-if = mostrar>
+      <p>Selecione o nome que deseja cancelar:</p>
+      <select v-model="cancSelecionado">
+        <option class="selectCancelamento" v-for="reserva of reservas" :value="reserva">{{ reserva }}</option>
+      </select>
+      <button class="btnCancelar" @click="cancelar">Confirmar</button>
+    </div>
   </div>
 </template>
 
@@ -22,32 +25,45 @@ export default {
     return {
       nome: '',
       data: '',
-      reservas: ['Diego', 'Aline', 'Carlos', 'Mariana' ]
+      reservas: ['Aline', 'Fernando', 'Claudio', 'Amanda', 'Douglas', 'Renata', 'Mariana', 'Orlando', 'Margareth'],
+      listaEspera: [],
+      mostrar: false,
+      cancSelecionado: ''
     }
   },
   methods: {
       reservar() {
-          const novaReserva = this.nome
-          this.reservas.push(novaReserva)
+            if(this.reservas.length < 10) {
+              const novaReserva = this.nome
+              this.reservas.push(novaReserva)
+              this.reservas.sort()
+            } else {
+            const confirmacao = confirm(`${this.nome}, o numero de vagas esgotou, deseja entrar na lista de espera ?`)
+            if(confirmacao) {
+              this.listaEspera.push(this.nome)
+              alert(`${this.nome}, você é a ${this.listaEspera.length}ª pessoa da lista de espera.`)
+            } 
+          }
           this.nome = '' 
           this.data = ''
       },
       cancelar() {
-        
-        alert('Para cancelar sua reserva, informe seu nome na caixa de texto a seguir.')
-        const nome = prompt('Qual seu nome ?')
-        const a = this.reservas.find(busca => busca === nome)
-        if(a) {
-          const busca = 
-          console.log(`${nome}, sua reserva foi cancelada.`)
-        } else {
-          console.log('Nome não encontrado.')
+        const confirmacao = confirm(`${this.cancSelecionado}, deseja realmente cancelar sua reserva ?`)
+        const index = this.reservas.indexOf(this.cancSelecionado)
+        if(confirmacao){
+          this.reservas.splice(index, 1)
+          this.reservas.sort()
+          if(this.listaEspera.length > 0) {
+            const prioridade = this.listaEspera[0]
+            this.reservas.push(prioridade)
+            let index = this.listaEspera.indexOf(prioridade)
+            this.listaEspera.splice(index, 1)
+            this.reservas.sort()
+            alert(`${this.cancSelecionado} sua vaga foi cancelada e preenchida pelo(a) ${prioridade}.`)
+          }
         }
-      },
-      valida() {
-        const reservas = this.reservas.filter(this.data).length
-        console.log(reservas)
-      },
+        this.mostrar = false
+      },      
       novaData(data) {
         const arr = data.split('-')
         const dia = parseInt(arr[2])
@@ -60,9 +76,6 @@ export default {
         dataCompleta.push(dd)
         return new Date(...dataCompleta)
       }
-    },
-    computed: {
-
     }
   }
 </script>
@@ -85,6 +98,12 @@ export default {
   display: inline-block;
   width: 100px;
   margin-left: 50px;
+}
+.btnConfirmar {
+  margin-left: 5px;
+}
+.btnCancelar {
+  margin-left: 5px;
 }
 
 </style>
