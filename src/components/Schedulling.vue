b78d22185e2645cee39867be5aa2fc2ac69a8441
Diego Pousas <diego.vieira@demarco.com.br>
2023-10-10 08:46:24 -0300

    feat: reserve-tab

--- src/components/Schedulling.vue
+++ src/components/Schedulling.vue
@@ -6,20 +6,20 @@
       centered
       @hide="closedModal">
       <div id="modalReserve" centralized>
-        <b-card :title="tableData.name">
+        <b-card :title="modalData.title">
           <b-card-text>Data: {{ formatter(inputedDate) }}</b-card-text>
-          <b-card-text bold>Status: {{ tableData.avaiable == true ? 'Disponível' : 'Indisponível' }}</b-card-text>
+          <b-card-text bold>{{ modalData.avaiable == true ? 'Disponível' : 'Indisponível' }}</b-card-text>
         </b-card>
         <b-alert :variant="this.messageAlert.variant" :show="this.messageAlert.status" class="mt-2">{{ this.messageAlert.text }}</b-alert>
-        <!-- <b-form-input centralized type="text" v-model="inputedName" size="sm" class="mt-3" v-if="!reserveModalStyle.status" :placeholder="reserveModalStyle.message.placeName">{{ reserveModalStyle.message.name }}</b-form-input> -->
+        <b-form-input id="inpName" centralized type="text" v-model="inputedName" size="sm" class="mt-3" v-if="reserveModalStyle.inputStatus" :placeholder="reserveModalStyle.message.placeName">{{ reserveModalStyle.message.name }}</b-form-input>
         <b-form-group
         description="Digite somente os números do seu CPF"
         centralized
         class="mt-1">
         <b-input-group prepend="CPF" size="sm" class="mt-1">
-          <b-form-input centralized placeholder="Informe seu CPF para reservar esta mesa" type="text" v-model="inputedCPF" :disabled="!tableData.avaiable" maxlength="11"></b-form-input>
+          <b-form-input centralized placeholder="Informe seu CPF para reservar esta mesa" type="text" v-model="inputedCPF" :disabled="!modalData.avaiable" maxlength="11"></b-form-input>
           <b-input-group-append>
-              <b-button :variant="reserveModalStyle.variant" :disabled="reserveModalStyle.status">{{ reserveModalStyle.value }}</b-button>
+              <b-button :variant="reserveModalStyle.variant" :disabled="registerButtonReserveStatus" @click="register(reserveModalStyle.value)">{{ reserveModalStyle.value }}</b-button>
             </b-input-group-append>
           </b-input-group>
           <p class="message" v-if="reserveModalStyle.status">{{ reserveModalStyle.message.text }}</p>
@@ -34,30 +34,30 @@
     <div id="groupSeasons">
       <b-row>
         <b-col md="6">
-          <b-card id="seasons" v-for="(season, i) in tabless" :key="i" v-show="i < 5">
+          <b-card id="seasons" v-for="(season, i) in tables" :key="i" v-show="i < 5" @click="preReserve(season.tableName, season.avaiable, season)">
             <b-row>
               <b-col md="1">
-                <b-button id="status" :variant="buttonStatus(season.person)" @click="preReserve(season.name, season.avaiable, season)"></b-button>
+                <b-button id="status" :variant="buttonStatus(season.person)"></b-button>
               </b-col>
               <b-col md="6">
-                {{ season.name }}
+                {{ season.tableName }}
               </b-col>
-              <b-col md="4">
+              <b-col md="4" small>
                 {{ season.person ? 'Indisponível' : 'Disponível' }}
               </b-col>
             </b-row>
           </b-card>
         </b-col>
         <b-col md="6">
-          <b-card id="seasons" v-for="(season, i) in tabless" :key="i" v-show="i > 4">
+          <b-card id="seasons" v-for="(season, i) in tables" :key="i" v-show="i > 4" @click="preReserve(season.tableName, season.avaiable, season)">
             <b-row>
               <b-col md="1">
-                <b-button id="status" :variant="buttonStatus(season.person)" @click="preReserve(season.name, season.avaiable, season)"></b-button>
-              </b-col>
-              <b-col md="6">
-                {{ season.name }}
+                <b-button id="status" :variant="buttonStatus(season.person)"></b-button>
               </b-col>
               <b-col md="5">
+                {{ season.tableName }}
+              </b-col>
+              <b-col md="6" small>
                 {{ season.person ? 'Indisponível' : 'Disponível' }}
               </b-col>
             </b-row>
@@ -81,26 +81,27 @@ export default {
       inputedCPF: '',
       reserves: [],
       search: [],
-      tabless: [],
+      tables: [],
       buttonColor: 'success',
-      tableData: {
-        name: '',
+      modalData: {
+        title: '',
         avaiable: '',
         date: ''
       },
       messageAlert: {
+        status: false,
         text: '',
-        variant: 'success',
-        status: false
+        variant: 'success'
       },
       reserveModalStyle: {
+        inputStatus: false,
         status: true,
         variant: 'info',
         value: 'Reservar',
         message: {
           status: false,
           text: '',
-          placeName: 'Seu nome aparecerá aqui...',
+          placeName: 'Informe aqui seu nome...',
           name: ''
         }
       }
@@ -113,6 +114,13 @@ export default {
       const m = String(date.getMonth() + 1).padStart(2, '0')
       const d = String(date.getDate()).padStart(2, '0')
       return `${y}-${m}-${d}`
+    },
+    registerButtonReserveStatus() {
+      if(this.inputedName.length < 4) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   created() {
@@ -128,6 +136,27 @@ export default {
       return new Date(date).toLocaleDateString('pt-BR')
     },
 
+    register(value) {
+      if(value === 'Reservar') {
+        const indData = Object.values(this.reserves).findIndex((p) => p.data === this.inputedDate)
+        const indPerson = Object.values(this.collaborators).findIndex((p) => p.cpf === this.inputedCPF)
+        const indTable = Object.values(this.reserves)[indData].tables.findIndex((p) => p.name === this.modalData.title)
+        this.$http.update(Object.values(this.reserves)[indData].tables[indTable].person, Object.values(this.collaborators)[indPerson].name)
+        console.log(Object.values(this.reserves)[indData].tables[indTable].person)
+      } else if(value === 'Cadastrar') {
+        this.$http.post('collaborators.json', {cpf: this.inputedCPF, name: this.inputedName})
+          .then(() => {
+            this.dbConnectCollaborators()
+            this.reserveModalStyle.inputStatus = false
+            this.messageAlert.text = `${this.inputedName}, seu cadastro foi concluido com sucesso. Para concluir sua reserva, clique em reservar.`
+            this.messageAlert.status = true
+            this.modalData.avaiable = false
+            this.reserveModalStyle.variant = 'info'
+            this.reserveModalStyle.value = 'Reservar'
+          })        
+      }
+    },
+
     isRegistred() {
       let test = Object.values(this.collaborators).findIndex((p) => p.cpf === this.inputedCPF)
       if(test != '-1') {
@@ -135,17 +164,21 @@ export default {
         this.messageAlert.status = true
         this.messageAlert.variant = 'success'
         this.messageAlert.text = `${name}, confira seu CPF e conclua a reserva.`
+        this.reserveModalStyle.value = 'Reservar'
         this.reserveModalStyle.message.name = name
+        this.reserveModalStyle.variant = 'info'
         this.reserveModalStyle.status = false
+        this.inputedName = name
         return true
       } else {
-        this.reserveModalStyle.status = true
+        this.inputedName = ''
         this.messageAlert.status = true
         this.messageAlert.variant = 'warning'
-        this.messageAlert.text = 'O CPF digitado ainda não tem cadastro na nossa base, clique no botão cadastrar finalizar o cadastro.'
+        this.messageAlert.text = 'O CPF digitado ainda não tem cadastro na nossa base. Certifique que o mesmo esteja correto, informe seu nome no campo indicado e clique em cadastrar.'
         this.reserveModalStyle.value = 'Cadastrar'
         this.reserveModalStyle.variant = 'success'
         this.reserveModalStyle.status = false
+        this.reserveModalStyle.inputStatus = true
         return false
       }
     },
@@ -168,12 +201,11 @@ export default {
         this.workSeasons.data = this.inputedDate
         this.workSeasons.tables = []
         for(let i = 0; i < 10; i++) {
-          this.workSeasons.tables.push({ name: `Mesa ${i+1}`, avaiable: true, person: ''})
+          this.workSeasons.tables.push({ tableName: `Mesa ${i+1}`, avaiable: true, person: ''})
         }
         this.$http.post('reserves.json', this.workSeasons)
-        setTimeout(() => {
-          this.dbConnectReserves()        
-        }, 2000);
+          this.dbConnectReserves()
+          this.tables = this.workSeasons.tables     
       } else {
         this.dbConnectReserves()
       }
@@ -189,22 +221,28 @@ export default {
     
     preReserve(name, status, date) {
       this.$refs.modalReserve.show()
-      this.tableData.name = name
-      this.tableData.avaiable = status
-      this.tableData.date = date
+      this.modalData.title = name
+      this.modalData.avaiable = status
+      this.modalData.date = date
     },
 
     closedModal() {
       this.inputedCPF = ''
+      this.inputedName = ''
+      this.reserveModalStyle.inputStatus = false
       this.messageAlert.status = false
     }
   },
   watch: {
     'inputedDate': function() {
       const data = Object.values(this.reserves)
-      const c = data.findIndex((p) => p.data === this.inputedDate)
-      const selected = Object.values(data)[c]
-      selected ? this.tabless = (Object.values(selected)[1]) : this.createSeasons()
+      const indDate = data.findIndex((p) => p.data === this.inputedDate)
+      const selected = Object.values(data)[indDate]
+      if(selected) {
+        this.tables = (Object.values(selected)[1])
+      } else {
+        this.createSeasons()                
+      }
     },
     'inputedCPF': function() {
       if(this.inputedCPF.length === 0) {
@@ -269,6 +307,11 @@ export default {
   margin: auto;
   margin-top: 5px;
   box-shadow: 2px 2px 2px rgb(179, 179, 179);
+  cursor: pointer;
+}
+
+[small] {
+  font-size: 15px;
 }
 
 
